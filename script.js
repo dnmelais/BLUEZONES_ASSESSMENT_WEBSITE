a30690b0ed9624c48e85e37970cd067dc0768df8
Nadia Melaisis <dnmelais22@gmail.com>
2023-11-28 18:44:04 -0500

    added REST countries API & changed color schemes

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ showDialog = (city) => {
     // get the city data from the array above using the identifier that was passed in.
     const cityContent = cityData.find(c => city == c.city_id);
 
+
     console.log(`Showing dialog for city: ${cityContent.city_name}`);
 
     const dialogContentContainer = document.getElementById("content-container");
@@ -75,3 +76,39 @@ function evaluateSurvey() {
         "Your city is an excellent candidate for the Blue Zones Project!" :
         "Your city may not be the best fit for the Blue Zones Project.";
 }
+// this is an arrow function
+ const loadCountryAPI = () =>{
+    // fetch url of rest countries from website
+        fetch('https://restcountries.com/v3.1/all')
+          .then(res => res.json())
+          .then(data => displayCountries(data))
+          .catch(error => {
+            console.error('Error fetching data:', error);
+          });
+      };
+      
+      // call the function to get output in the console
+      loadCountryAPI();
+    
+
+ //displaying all countries
+ const displayCountries = countries =>{
+        console.log(countries);
+        const countriesHTML = countries.map(country => getCountry(country))
+        // displaying div to html
+        const container = document.getElementById('countries');
+        container.innerHTML = countriesHTML.join('');
+    }
+    const getCountry = (country) => {
+        console.log(country);
+        return `
+          <div class="country-div">
+            <img src="${country.flags.png}">
+            <h2>${country.name.common}</h2>
+            <h4>Population: ${country.population}</h4>
+            <h4>Region: ${country.region}</h4>
+            <h4>Capital: ${country.capital}</h4>
+          </div>
+        `;
+      };
+      
